996a93c50bee223417b3d9f58bc26df960f160dc
Nadir <nadir.casciola@gmail.com>
2020-08-26 23:51:45 +0200

    wip: extract features

--- model.py
+++ model.py
@@ -7,7 +7,8 @@ import keras
 import sys
 
 '''
-usage: python model.py [infer|extract] img_filename
+usage for prediction: python model.py infer img_filename
+usage for extraction: python model.py extract img_filename layerNumber
 
 temp links
 https://stackoverflow.com/questions/46828476/how-to-visualize-output-of-intermediate-layers-of-convolutional-neural-network-i
@@ -34,8 +35,11 @@ class Model:
         
         print('%s (%.2f%%)' % (label[1], label[2] * 100))
 
-    def extract(self, img_filename):
-        self.printLayers()
+    def extract(self, img_filename, layerNumber):
+        #self.printLayers()
+        newModel = keras.models.Model(self.model.inputs, self.model.layers[layerNumber].output)
+        #print(newModel.summary())
+        newModel.predict()
 
     def printLayers(self):
         i = 0
@@ -43,9 +47,18 @@ class Model:
             print(str(i) + ': ', l.name, l.output_shape)
             i = i + 1
 
+    def input_preprocess(self, img_filename):
+        image = load_img(img_filename, target_size=(224, 224))
+        
+        image = img_to_array(image)
+        
+        image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
+        
+        image = preprocess_input(image)
+
 
 m = Model()
 if sys.argv[1] == 'infer':
     m.infer(sys.argv[2])
 elif sys.argv[1] == 'extract':
-    m.extract(sys.argv[2])
+    m.extract(sys.argv[2], int(sys.argv[3]))
