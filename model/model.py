58637789825c5da1af12aae6d670ee4ee6d31129
Diddy42 <nadir.casciola@gmail.com>
2020-10-03 13:09:01 +0200

    feat: make the model output 3 images for each (significant) layer

--- model/model.py
+++ model/model.py
@@ -13,8 +13,10 @@ import matplotlib.pyplot as plt
 usage for prediction: python model.py infer img_filename
 es. python model.py infer elephant.jpg
 
-usage for extraction: python model.py extract img_filename layerNumber
-es. python model.py extract elephant.jpg 4
+usage for extraction: python model.py extract img_filename
+es. python model.py extract elephant.jpg
+
+extraction: it extracts 3 images for each significant layer, and saves them in a directory
 
 temp links
 https://stackoverflow.com/questions/46828476/how-to-visualize-output-of-intermediate-layers-of-convolutional-neural-network-i
@@ -32,40 +34,43 @@ class Model:
         
         print('%s (%.2f%%)' % (label[1], label[2] * 100))
 
-    def extract(self, img_filename, layerNumber):
+    def extract(self, img_filename):
         self.printLayers()
-        newModel = keras.models.Model(self.model.inputs, self.model.layers[layerNumber].output)
-        #print(newModel.summary())
+        significant_layers = [2, 5, 9, 13, 17, 18]
 
-        img = self.input_preprocess(img_filename)
-        pred = newModel.predict(img)
-        numFilters = pred.shape[3]
-        print('The output of the chosen layer is: ', pred.shape, ', so there are ', numFilters, ' images as output of this layer. Choosing some at random...')
+        for sl in significant_layers:
+            print('now doing layer ' + str(sl))
+            newModel = keras.models.Model(self.model.inputs, self.model.layers[sl].output)
 
-        pred = np.squeeze(pred)
-        #now pred has shape like (112, 112, 128), which correspond to 128 images 112x112
-        pred = np.dsplit(pred, numFilters)
-        #now pred is a list of 112x112 images  (or whatever specific number)
+            img = self.input_preprocess(img_filename)
+            pred = newModel.predict(img)
+            numFilters = pred.shape[3]
+            print('The output of the chosen layer is: ', pred.shape, ', so there are ', numFilters, ' images as output of this layer. Choosing some at random...')
 
-        
-        
-        n = 3
-        for i in range(n):
-            r = random.randint(0, numFilters - 1)
+            pred = np.squeeze(pred)
+            #now pred has shape like (112, 112, 128), which correspond to 128 images 112x112
+            pred = np.dsplit(pred, numFilters)
+            #now pred is a list of 112x112 images  (or whatever specific number)
+            
+            n = 3
+            for i in range(n):
+                print('now doing image ' + str(i))
+                r = random.randint(0, numFilters - 1)
 
-            #arr_img = pred[:, :, r]       
-            arr_img = pred[r]                   #now only select one at random
-            arr_img = np.squeeze(arr_img)
+                #arr_img = pred[:, :, r]       
+                arr_img = pred[r]                   #now only select one at random
+                arr_img = np.squeeze(arr_img)
 
-            #now make sure all the data in the array is in the range (0, 255)
-            maxa = np.amax(arr_img)
-            arr_img = arr_img / maxa
-            arr_img = arr_img * 255 
+                #now make sure all the data in the array is in the range (0, 255)
+                maxa = np.amax(arr_img)
+                if maxa != 0:
+                    arr_img = arr_img / maxa
+                    arr_img = arr_img * 255 
 
-            plt.figure()
-            plt.imshow(arr_img)
-            plt.savefig('./extract_output/' + str(i) + '.png')
-            plt.close()
+                plt.figure()
+                plt.imshow(arr_img)
+                plt.savefig('./extract_output/' + 'l'+str(sl)+'_' + str(i) + '.png')
+                plt.close()
         
 
     def printLayers(self):
@@ -84,8 +89,8 @@ class Model:
 
 
 m = Model()
-print(m.model.summary())
+#print(m.model.summary())
 if sys.argv[1] == 'infer':
     m.infer(sys.argv[2])
 elif sys.argv[1] == 'extract':
-    m.extract(sys.argv[2], int(sys.argv[3]))
+    m.extract(sys.argv[2])
